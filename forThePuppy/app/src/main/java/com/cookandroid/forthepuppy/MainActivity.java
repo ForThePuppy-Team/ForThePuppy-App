ba9854ba741caec044c4ec025a339a29a639703a
이시연 <75464328+leesiyeon3152@users.noreply.github.com>
2022-05-30 02:07:51 +0900

    test

--- forThePuppy/app/src/main/java/com/cookandroid/forthepuppy/MainActivity.java
+++ forThePuppy/app/src/main/java/com/cookandroid/forthepuppy/MainActivity.java
@@ -1,10 +1,15 @@
 package com.cookandroid.forthepuppy;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.cookandroid.forthepuppy.ui.chatting.ChattingFragment;
 import com.cookandroid.forthepuppy.ui.home.HomeFragment;
@@ -12,6 +17,9 @@ import com.cookandroid.forthepuppy.ui.my_page.MyPageFragment;
 import com.cookandroid.forthepuppy.ui.surrounding_facilities.SurroundingFacilitiesFragment;
 import com.cookandroid.forthepuppy.ui.walk.WalkFragment;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     HomeFragment homeFragment;
@@ -75,6 +83,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // 해시키
+//        getHashKey();
     }
+
+    // 해시키 구하기
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
 }
--- forThePuppy/app/src/main/java/com/cookandroid/forthepuppy/ui/surrounding_facilities/SurroundingFacilitiesFragment.java
+++ forThePuppy/app/src/main/java/com/cookandroid/forthepuppy/ui/surrounding_facilities/SurroundingFacilitiesFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.view.ViewGroup;
 
 import com.cookandroid.forthepuppy.MainActivity;
 import com.cookandroid.forthepuppy.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class SurroundingFacilitiesFragment extends Fragment {
 
@@ -23,6 +27,21 @@ public class SurroundingFacilitiesFragment extends Fragment {
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.surroundingFacilitiesToolbar);
         MainActivity activity = (MainActivity) getActivity();
         activity.setSupportActionBar(toolbar);
+
+        ViewPager vp = (ViewPager) view.findViewById(R.id.viewpager);
+        VPAdapter adapter = new VPAdapter(getActivity().getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        // 탭바랑 연동
+        TabLayout tab = (TabLayout) view.findViewById(R.id.sf_tab);
+        tab.setupWithViewPager(vp);
+
+        ArrayList<Integer> images = new ArrayList<>();
+        images.add(R.drawable.ic_baseline_home_24);
+        images.add(R.drawable.ic_baseline_person_24);
+        images.add(R.drawable.ic_baseline_forum_24);
+
+        for (int i = 0; i < 3; i++) tab.getTabAt(i).setIcon(images.get(i));
         return view;
     }
 }
